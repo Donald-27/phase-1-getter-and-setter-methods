55a5afca4cfab45ee41b66ea9fa90ea20748b328
Donald-27 <+>
2025-03-29 17:50:20 +0300

    added circle class to the index.js

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your Circle class here
+class Circle {
+    constructor(radius) {
+      this.radius = radius;
+    }
+  
+    // Getter for diameter
+    get diameter() {
+      return this.radius * 2;
+    }
+  
+    // Setter for diameter (sets radius based on diameter)
+    set diameter(diameter) {
+      this.radius = diameter / 2;
+    }
+  
+    // Getter for circumference
+    get circumference() {
+      return Math.PI * this.diameter;
+    }
+  
+    // Setter for circumference (sets radius based on circumference)
+    set circumference(circumference) {
+      this.radius = circumference / Math.PI / 2;
+    }
+  
+    // Getter for area
+    get area() {
+      return Math.PI * Math.pow(this.radius, 2);
+    }
+  
+    // Setter for area (sets radius based on area)
+    set area(area) {
+      this.radius = Math.sqrt(area / Math.PI);
+    }
+  }
+  
+  module.exports = Circle;
+  
